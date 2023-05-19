6fa94c781f10a527c81e216c9c187f99b37eacb1
Lcs12377 <lcsbooks1@gmail.com>
2023-05-18 22:21:05 -0300

    Adicionar arquivos de projeto.

---
+++ Listadepessoas/NovaPasta/Lista.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listadepessoas.NovaPasta
+{
+    internal class Lista
+    {
+        public int Id { get; set; }
+        public String? nomeList { get; set; }
+        public string nome { get; set; }
+        public string? sobrenome { get; }
+        public DateTime datanascimento
+        {
+            get; set;
+        }
+        public string? email { get; set; }
+        public string? cpf { get; set; }
+        public string? fone { get; set; }
+        public string? sexo { get; set; }
+        public string? estadocivil { get; set; }
+        public string? hobbie { get; set; }
+        public string? idade { get; set; }
+
+        
+            
+    }
+}
---
+++ Listadepessoas/NovaPasta1/Gerar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listadepessoas.NovaPasta1
+{
+    internal class Gerar
+    {
+       {
+        public string Gerarnome(int Inicio, int Fim, int Qtde)
+            string OUrlPesquisa = "https://localhost:7120/ListadePessoas?IdadeInicial=" + Inicio.ToString() + "&IdadeFinal=" + Fim.ToString()
+}
+
+    }
+}
